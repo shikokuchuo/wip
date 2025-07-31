09e4f74853662f10c40f8311aa7f1bfee0186e16
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-07-31 11:04:52 +0100

    Add run_now(all = FALSE)

--- promises.R
+++ promises.R
@@ -1,6 +1,8 @@
 library(promises)
 library(mirai)
 
+# Synchronous completion blocks Shiny reactivity:
+
 daemons(1)
 
 promise_resolve(0) |> then(print)
@@ -17,5 +19,26 @@ while (!later::loop_empty()) {later::run_now(Inf); print("Shiny Reactivity")}
 y <- 0
 mirai(1) |> then(\(x) y <<- x) |> then(\(x) print(y - x))
 while (!later::loop_empty()) {later::run_now(Inf); print("Shiny Reactivity")}
+
+daemons(0)
+
+# Synchronous completion allows Shiny reactivity if `run_now(all = FALSE)`:
+
+daemons(1)
+
+promise_resolve(0) |> then(print)
+mirai(1) |> then(print) |> then(print) |> then(print)
+mirai(2) |> then(print) |> then(print)
+promise_resolve(3) |> then(print)
+while (!later::loop_empty()) {later::run_now(Inf, all = FALSE); print("Shiny Reactivity")}
+
+y <- 0
+mirai(1) |> then(\(x) y <<- x) |> then(\(x) print(y - x))
+mirai(2) |> then(\(x) y <<- x) |> then(\(x) print(y - x))
+while (!later::loop_empty()) {later::run_now(Inf, all = FALSE); print("Shiny Reactivity")}
+
+y <- 0
+mirai(1) |> then(\(x) y <<- x) |> then(\(x) print(y - x))
+while (!later::loop_empty()) {later::run_now(Inf, all = FALSE); print("Shiny Reactivity")}
 
 daemons(0)
